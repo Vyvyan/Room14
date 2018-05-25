2cd5311010a6b210eb72df2bb539b8ff912ad791
Vyv <Vyvvvv>
2018-05-25 12:32:51 -0700

    added a simple scene to enable the second monitor.

---
+++ Assets/SetupMultiMon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetupMultiMon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (Display.displays.Length > 1)
+            Display.displays[1].Activate();
+        if (Display.displays.Length > 2)
+            Display.displays[2].Activate();
+
+        SceneManager.LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
